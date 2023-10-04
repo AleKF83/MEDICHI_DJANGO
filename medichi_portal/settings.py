82cec54484ee18e8ea5d9497ecf3c8eff5981ced
AleKF83 <ale_programacion@outlook.com>
2023-10-04 17:49:47 -0300

    db

--- medichi_portal/settings.py
+++ medichi_portal/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = "medichi_portal.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
+        "NAME": "medichi_db",
+        "USER": "postgres",
+        "PASSWORD": "alitoooo1234",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
